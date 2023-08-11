34d044be321dfedab22c6b0f2198e6c3bb33ec1a
Kulagin Yuri <jkulvichi@gmail.com>
2023-08-11 14:15:53 +0300

    .
    Signed-off-by: Kulagin Yuri <jkulvichi@gmail.com>

--- app/assets/js/distromanager.js
+++ app/assets/js/distromanager.js
@@ -2,8 +2,6 @@ const { DistributionAPI } = require('helios-core/common')
 
 const ConfigManager = require('./configmanager')
 
-// Old WesterosCraft url.
-// exports.REMOTE_DISTRO_URL = 'http://mc.westeroscraft.com/WesterosCraftLauncher/distribution.json'
 exports.REMOTE_DISTRO_URL = 'https://raw.githubusercontent.com/mncred/launcher/master/distribution.json'
 
 const api = new DistributionAPI(
